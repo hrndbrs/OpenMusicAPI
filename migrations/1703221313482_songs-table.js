29aef3aaeb4ddf37acab200c08aad5935fb65628
hrndbrs <hrndbrs@gmail.com>
2023-12-22 12:26:21 +0700

    add songs table migration

---
+++ migrations/1703221313482_songs-table.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = pgm => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      primaryKey: true
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    genre: {
+      type: 'VARCHAR(255)',
+      notNull: true
+    },
+    performer: {
+      type: 'VARCHAR(255)',
+      notNull: true
+    },
+    duration: 'INTEGER',
+    albumId: {
+      type: 'VARCHAR(255)',
+      references: 'albums',
+      onDelete: 'SET NULL',
+      onUpdate: 'SET NULL'
+    }
+  })
+}
+
+exports.down = pgm => {
+  pgm.dropTable('songs')
+}
