874427a55b80cbb54efea602206b5414bc55fdd3
hrndbrs <hrndbrs@gmail.com>
2024-01-07 21:31:35 +0700

    minor fix-7

--- src/services/service.js
+++ src/services/service.js
@@ -1,22 +1,10 @@
-const { Pool } = require('pg')
+const pool = require('../config/pg')
 
 module.exports = class Service {
   #pool
 
   constructor () {
-    this.#pool = this.connectToDatabase()
-  }
-
-  connectToDatabase () {
-    const pool = new Pool({
-      port: process.env.PGPORT,
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-      password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE
-    })
-
-    return pool
+    this.#pool = pool
   }
 
   get pool () {
